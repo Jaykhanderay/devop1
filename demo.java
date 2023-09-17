b69cb48d4237f2a81dbb9608f65ba574b95c908a
elliots234 <143217153+elliots234@users.noreply.github.com>
2023-09-17 17:03:32 +0530

    Add files via upload

--- demo.java
+++ demo.java
@@ -1,7 +1,29 @@
+import java.util.*;
+
 public class demo
 {
-public static void main(String args[])
-{
-System.out.println("hi i am  jay andi am study the devops now")
-}
+    public static void main(String args[])
+    {
+         int[]  arr = {5,4,3,2,1};
+         bubble(arr);
+         System.out.println(Arrays.toString(arr));
+    }
+
+    static void bubble(int[] arr)
+    {
+
+       // int temp =0;
+        for(int i=0;i<arr.length;i++)
+        {
+            for(int j=1;j<arr.length-i;j++)
+            {
+                if (arr[j]<arr[j-1])
+                {
+                   int temp = arr[j];
+                    arr[j]= arr[j-1];
+                    arr[j-1]=temp;
+                }
+            }
+        }
+    }
 }
