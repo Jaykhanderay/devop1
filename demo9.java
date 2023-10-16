970b6cb92bb0164d1122d951cef368d0f8e9b2c2
jay4578 <khanderayjay672@gmail.com>
2023-10-16 10:53:38 +0530

    file added

---
+++ demo9.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class demo9
+{
+    public static void main()
+    {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int [10];
+        int n = sc.nextInt();
+        
+        for (int i = 0; i<arr.length; i++)
+        {
+            System.out.println("Enter the number of elements");
+            arr[i]=sc.nextInt();
+
+        }
+
+    }
+}
